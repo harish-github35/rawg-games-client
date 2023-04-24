ea23dbc74e292e9bb90d83aefa069e8ac6671724
harish.innbox35 <harish.inbox35@gmail.com>
2023-04-24 19:20:11 +0530

    move api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,14 @@
 import axios, { CanceledError } from "axios";
 
+const key =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_KEY
+    : import.meta.env.API_KEY;
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "4fb38a1575ff4a41a27be3585911b3f1",
+    key: key,
   },
 });
 
